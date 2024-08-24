20723efed2b6c811b83613254a2fe31f50095f4a
fabyup <afabyblasco@gmail.com>
2024-08-23 21:24:49 -0300

    agregue conexion.js

--- conexion.js
+++ conexion.js
@@ -1,2 +1,16 @@
 const{createConnection} = require("mysql2/promise");
 
+
+
+async function main() {
+  const conexionDB = await createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345",
+    database: "MAYORISTA"
+  });
+  const resultado = await conexionDB.query("SELECT * FRom productos");
+  conexionDB.end();
+  console.log(resultado);
+}
+main();
